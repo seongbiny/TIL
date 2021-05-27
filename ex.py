6179b3d2562684af3f275398c968775faa65b8c5
Yun Seongbin <s30ngb1n@gmail.com>
2021-05-27 16:54:59 +0900

    계산 문제를 맞히는 게임

--- ex.py
+++ ex.py
@@ -5,18 +5,18 @@ def make_question():
     b = random.randint(1, 20)
     op = random.randint(1, 3)
 
-q = str(a)
+    q = str(a)
 
-if op == 1:
-    q = q + "+"
-if op == 2:
-    q = q + "-"
-if op == 3:
-    q = q + "*"
+    if op == 1:
+        q = q + "+"
+    if op == 2:
+        q = q + "-"
+    if op == 3:
+        q = q + "*"
 
-q = q + str(b)
+    q = q + str(b)
 
-return q
+    return q
 
 sc1 = 0
 sc2 = 0
