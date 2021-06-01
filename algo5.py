0ab3d4639d965152af439c8490f73222db0e11b6
Yun Seongbin <s30ngb1n@gmail.com>
2021-06-01 22:34:38 +0900

    601

---
+++ algo5.py
@@ -0,0 +1,9 @@
+def fact(n):
+    f = 1
+    for i in range(1, n+1):
+        f = f * i
+    return f
+
+print(fact(1))
+print(fact(5))
+print(fact(10))
---
+++ algo6.py
@@ -0,0 +1,13 @@
+def hanoi(n, from_pos, to_pos, aux_pos):
+    if n == 1:
+        print(from_pos, "->", to_pos)
+        return
+
+    hanoi(n-1, from_pos, aux_pos, to_pos)
+    print(from_pos, "->", to_pos)
+    hanoi(n-1, aux_pos, to_pos, from_pos)
+
+print("n = 1")
+
+hanoi(1, 1, 3, 2)
+print
