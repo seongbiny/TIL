8dd5a88760910f9b45a992070e2cee401fe47451
seongbiny <s30ngb1n@gmail.com>
2021-11-01 22:12:38 +0900

    211101

---
+++ Javascript/JS_you/day_03/01_blocking.py
@@ -0,0 +1,23 @@
+# 1. sleep
+from time import sleep
+
+
+def sleep_3_seconds():
+    sleep(3)
+    print('잘 잤다!!!')
+
+print('이제 자야지')
+sleep_3_seconds()
+print('학교 가자!!!')
+
+
+
+# 2. requests
+
+import requests
+
+response = requests.get('https://jsonplaceholder.typicode.com/todos/1/')
+
+todo = response.json()
+
+print(todo)
